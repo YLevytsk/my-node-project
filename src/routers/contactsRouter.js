e6c5f4077cf10bfb50683c5f29a7d3da0dde4133
yuliia levytska <lupin773311asd@gmail.com>
2025-06-15 10:01:58 +0100

    ваів

--- src/routers/contactsRouter.js
+++ src/routers/contactsRouter.js
@@ -8,17 +8,19 @@ import {
   patchContactController,
 } from '../controllers/contactsController.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = express.Router();
 
 router.get('/', ctrlWrapper(getAllContactsController));
-router.get('/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 router.post('/', ctrlWrapper(createContactController));
-router.delete('/:contactId', ctrlWrapper(deleteContactController));
-router.put('/:contactId', ctrlWrapper(updateContactController));
-router.patch('/:contactId', ctrlWrapper(patchContactController));
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
+router.put('/:contactId', isValidId, ctrlWrapper(updateContactController));
+router.patch('/:contactId', isValidId, ctrlWrapper(patchContactController));
 
 export default router;
+
 
 
 
