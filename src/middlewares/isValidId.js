f6edc22c6c5df91b0f4d9f8584dceb69808c915c
yuliia levytska <lupin773311asd@gmail.com>
2025-06-15 10:06:41 +0100

    пв

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,11 +1,12 @@
 import { isValidObjectId } from "mongoose";
-import { HttpError } from "../utils/HttpError.js";
 
 export const isValidId = (req, res, next) => {
   const { contactId } = req.params;
+
   if (!isValidObjectId(contactId)) {
-    return next(HttpError(400, `${contactId} is not a valid id`));
+    return res.status(400).json({ message: `${contactId} is not a valid id` });
   }
+
   next();
 };
 
