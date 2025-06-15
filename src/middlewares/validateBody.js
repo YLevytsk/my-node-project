8b5fadc45f0b21a5af43426974dc4fe138fdd8cf
yuliia levytska <lupin773311asd@gmail.com>
2025-06-15 10:07:20 +0100

    ртопр

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,11 +1,11 @@
-import { HttpError } from "../utils/HttpError.js";
-
 export const validateBody = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.body);
+
     if (error) {
-      return next(HttpError(400, error.message));
+      return res.status(400).json({ message: error.message });
     }
+
     next();
   };
 };
