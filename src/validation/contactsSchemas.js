6f8c3da1325f6776e2056babc463891a3c3d5157
yuliia levytska <lupin773311asd@gmail.com>
2025-06-16 12:02:39 +0100

    квпвп

--- src/validation/contactsSchemas.js
+++ src/validation/contactsSchemas.js
@@ -2,17 +2,24 @@ import Joi from "joi";
 
 const name = Joi.string().min(3).max(20).required();
 const email = Joi.string().email().min(3).max(20).required();
-const phone = Joi.string().min(3).max(20).required();
+const phoneNumber = Joi.string().min(3).max(20).required();
+const isFavourite = Joi.boolean().optional();
+const contactType = Joi.string().valid("personal", "work", "other").optional();
 
 export const addContactSchema = Joi.object({
   name,
   email,
-  phone,
+  phoneNumber,
+  isFavourite,
+  contactType,
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   email: Joi.string().email().min(3).max(20),
-  phone: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid("personal", "work", "other"),
 }).min(1);
+
 
